fcda00cce301cb16062d48fdc301d7fb73f4bdea
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-11-23 15:15:49 -0600

    Scope global

---
+++ src/04-scope-global.js
@@ -0,0 +1,9 @@
+let greeting = 'Hello world';
+
+function greet() {
+    console.log(greeting);
+}
+
+greet();
+
+// Cuando la variable esta declarada en el ambito global, se puede acceder desde cualquier parte de la aplicacion.
