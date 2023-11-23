1c271c0e168e4ad7eb187a7c5f8ab42fed1a32f1
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-11-23 15:24:43 -0600

    Scope bloque

---
+++ src/06-scope-bloque.js
@@ -0,0 +1,10 @@
+{
+    let greeting = 'Hello world';
+    var lang = 'English';
+    console.log('bloque', greeting);
+}
+
+console.log('global', lang);
+console.log('global', greeting);
+
+// Para usar el ámbito de bloque, no solo hay que usar las llaves { } sino también las palabras reservadas let o const.
