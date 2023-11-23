c40bcb477f2d61002e5d19c2a7f46499108ff266
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-11-23 16:01:16 -0600

    Functions, declaration and expression

---
+++ src/08-funciones-declaracion-expresion.js
@@ -0,0 +1,15 @@
+// Declaracion de una funcion.
+function getResult() {
+    return "Results"
+}
+
+getResult(); // LLamado de la funcion
+console.log(getResult());
+
+// Expresion de una funcion.
+const getResult = function() {
+    return "Results"
+}
+
+getResult(); // LLamado de la funcion
+console.log(getResult());
